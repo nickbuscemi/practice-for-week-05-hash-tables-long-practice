8918e2ef47a34475c3fd4334fe79c2c2697cc737
Nick <nick.buscemi13@gmail.com>
2023-03-10 12:46:20 -0500

    finished practice problems. (could not solve two tests)

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,9 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,24 +33,98 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const index = this.hashMod(key);
+    
+    if (this.data[index] === null) {
+      this.data[index] = new KeyValuePair(key, value);
+    } else {
+      let current = this.data[index];
+      while (current.next !== null) {
+        if (current.key === key) {
+          current.value = value;
+          return;
+        }
+        current = current.next;
+      }
+      if (current.key === key) {
+        current.value = value;
+      } else {
+        current.next = new KeyValuePair(key, value);
+      }
+    }
+    
+    this.count++;
+    if (this.count > this.capacity * .75) {
+      this.resize();
+    }
   }
+  
 
-
+  
+  
+  
   read(key) {
-    // Your code here
+    const index = this.hashMod(key); // calculate the index using hashMod
+  
+    if (this.data[index] === null) { // if the index is null...
+      return undefined; // key does not exist so it should return undefined
+    } else { // if the index is not null...
+      let current = this.data[index]; // set a current pointer 
+  
+      while (current !== null) { // traverse the list using the current pointer until we find the key or reach the end of the list
+        if (current.key === key) { // if we find the key...
+          return current.value; // return its matching value
+        }
+        current = current.next; // if we don't find the key, move the current pointer to the next pointer
+      }
+  
+      return undefined; // if we reach the end of the list without finding the key then the key does not exist.. return undefined
+    }
   }
-
 
   resize() {
-    // Your code here
+    const oldData = this.data;
+    this.capacity *= 2;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
+  
+    for (let i = 0; i < oldData.length; i++) {
+      let current = oldData[i];
+      while (current !== null) {
+        this.insert(current.key, current.value);
+        current = current.next;
+      }
+    }
   }
-
 
   delete(key) {
-    // Your code here
+    const index = this.hashMod(key);
+  
+    if (this.data[index] === null) {
+      return "Key not found";
+    } else if (this.data[index].key === key) {
+      this.data[index] = this.data[index].next;
+      this.count--;
+      return "Key deleted";
+    } else {
+      let prev = this.data[index];
+      let current = this.data[index].next;
+  
+      while (current !== null) {
+        if (current.key === key) {
+          prev.next = current.next;
+          this.count--;
+          return "Key deleted";
+        }
+        prev = current;
+        current = current.next;
+      }
+  
+      return "Key not found";
+    }
   }
 }
+
 
 
 module.exports = HashTable;
--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,26 +1,129 @@
 function anagrams(str1, str2) {
   // Your code here
+  
+  //create a hash atble for each string
+  const hash1 = {};
+  const hash2 = {};
+
+  //iterate through each string
+  for (let i = 0; i < str1.length; i++) {
+    //if the character is already in the hash table, increment the frequency
+    if (hash1[str1[i]]) {
+      hash1[str1[i]]++;
+    //otherwise, add the character to the has table with a frequency of 1 
+    } else {
+      hash1[str1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (hash2[str2[i]]) {
+      hash2[str2[i]]++;
+    } else {
+      hash2[str2[i]] = 1;
+    }
+  }
+
+  for (let key in hash1) {
+    if (hash1[key] !== hash2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
   // Your code here
-}
+  const hash1 = {};
+  const hash2 = {};
+  let newArr = [];
 
+  for (let i = 0; i < arr1.length; i++) {
+    if (hash1[arr1[i]]) {
+      hash1[arr1[i]]++;
+    } else {
+      hash1[arr1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) { 
+    if (hash2[arr2[i]]) {
+      hash2[arr2[i]]++;
+    } else {
+      hash2[arr2[i]] = 1;
+    }
+  }
+
+  for (let key in hash1) {
+    if (hash2[key]) {
+      newArr.push(Number(key));
+    }
+  }
+  return newArr;
+}
+//console.log(commonElements([1, 3, 4, 5], [1, 2, 3, 5]));
 
 function duplicate(arr) {
   // Your code here
-}
+  const hash = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (hash[arr[i]]) {
+      hash[arr[i]]++;
+  } else {
+    hash[arr[i]] = 1;
+    }
+  } 
+  //console.log(hash)
 
+  for (key in hash) {
+    if (hash[key] === 2) {
+      return Number(key);
+    }
+  }
+  return 'duplicate not found'
+}
+//console.log(duplicate([1,2,3,4,5,6]))
 
 function twoSum(nums, target) {
   // Your code here
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (hash[complement]) {
+      return true;
+    }
+    hash[nums[i]] = i;
+  } 
+  return false;
+
 }
+//console.log(twoSum([2,7,11,15], 22))
+//console.log(twoSum([3,4,5], 6))
 
-
-function wordPattern(pattern, strings) {
-  // Your code here
+function wordPattern(pattern, strArr) {
+  const charMap = {};
+  const wordMap = {};
+  const words = strArr;
+  if (pattern.length !== words.length) {
+    return false;
+  }
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = words[i];
+    if (charMap[char] !== undefined) {
+      if (wordMap[word] !== charMap[char]) {
+        return false;
+      }
+    } else {
+      if (wordMap[word] !== undefined) {
+        return false;
+      }
+      charMap[char] = i;
+      wordMap[word] = i;
+    }
+  }
+  return true;
 }
-
+console.log(wordPattern("ABBA", ['dog', 'cat', 'cat', 'dog']));
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
